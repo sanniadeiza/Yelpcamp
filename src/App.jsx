d6983b4666ef21fcb975836c20e0b64ed4cfaf62
Sanniadeiza <sanniadeiza2019@gmail.com>
2026-04-09 17:56:21 +0100

    Allow environment variables for Amplify config

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,18 @@ import StarRating from './components/StarRating';
 import ReviewList from './components/ReviewList';
 import LocationMap from './components/LocationMap';
 
-Amplify.configure(awsConfig);
+const finalConfig = {
+  ...awsConfig,
+  aws_project_region: process.env.REACT_APP_REGION || awsConfig.aws_project_region || 'eu-north-1',
+  aws_cognito_region: process.env.REACT_APP_REGION || awsConfig.aws_cognito_region || 'eu-north-1',
+  aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID || awsConfig.aws_user_pools_id,
+  aws_user_pools_web_client_id: process.env.REACT_APP_USER_CLIENT_ID || awsConfig.aws_user_pools_web_client_id,
+  aws_appsync_graphqlEndpoint: process.env.REACT_APP_GRAPHQL_ENDPOINT || awsConfig.aws_appsync_graphqlEndpoint,
+  aws_appsync_region: process.env.REACT_APP_REGION || awsConfig.aws_appsync_region || 'eu-north-1',
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+};
+
+Amplify.configure(finalConfig);
 
 const initialState = {
   restaurants: [],
